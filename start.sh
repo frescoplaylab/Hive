c194f36ce1432a811a2b1bef8e0fc50d8596d066
frescoplaylab <38917706+frescoplaylab@users.noreply.github.com>
2019-02-08 17:09:47 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,11 +1,5 @@
 ssh-keygen -q -t rsa -P "" -f /root/.ssh/id_rsa 
 cat /root/.ssh/id_rsa.pub >> /root/.ssh/authorized_keys
 chmod 600 /root/.ssh/authorized_keys
-service ssh restart
-hdfs namenode -format
-start-all.sh 
-hdfs dfs -mkdir -p /user/hive/warehouse
-hdfs dfs -mkdir /tmp
-hdfs dfs -chmod g+w /user/hive/warehouse
-hdfs dfs -chmod g+w /tmp 
+service ssh restart 
 bash
